47db7aaa2095d5625d97a1a90978d4f2b67b67c3
chispainnov <chispainnov@gmail.com>
2025-09-16 22:08:44 -0400

    Update: latest changes to MVP

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/leo/oracle_mvp/.venv
+set -gx VIRTUAL_ENV /home/leo/oracle-mvp/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
